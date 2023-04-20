bd4048e5379d5ded5b0d2a0d7e9a3a5895727ff0
Asma Hassan <asmasheikhhassan@gmail.com>
2023-04-20 19:57:22 +0100

    added js file

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ button.addEventListener("click",function(event){
 
 async function getRecipeData(searchValue) {
     // document.getElementById('#recipe-search"').style.display = 'block';
-    var response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public?q=${searchValue}&app_id=96578cf4&app_key=ef31306002be0ec63784488c0c25e2ce&imageSize=REGULAR&imageSize=SMALL`);
+    var response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public/?q=${searchValue}&app_id=96578cf4&app_key=ef31306002be0ec63784488c0c25e2ce&imageSize=REGULAR&imageSize=SMALL`);
     const data = await response.json();
     console.log(data);
     return data;
