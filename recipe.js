77cfbbe8c3b2140dabbb83e28697e3c6f5e3b133
Asma Hassan <asmasheikhhassan@gmail.com>
2023-04-20 21:02:47 +0100

    Added js code

--- recipe.js
+++ recipe.js
@@ -8,17 +8,19 @@ var search = document.getElementById("search-input")
 
 button.addEventListener("click",function(event){
     event.preventDefault();
-    // var currentVal = search.val();
+    var currentVal = search.val();
 
-    getRecipeData("pasta")
+    //getRecipeData("search")
 
+    getRecipeData(currentVal).then(function(data){
+        displayWeatherData(data);
+    });
 
 });
 
 
 async function getRecipeData(searchValue) {
    // document.getElementsByClassName('card').style.display = 'block';
-   // var response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchValue}&app_id=96578cf4&app_key=ef31306002be0ec63784488c0c25e2ce&imageSize=REGULAR&imageSize=SMALL`);
     var response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=96578cf4&app_key=ef31306002be0ec63784488c0c25e2ce&q=${searchValue}`)
     console.log(response)
    const data = await response.json();
