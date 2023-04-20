2c414fa984c416cbdb2fb9181cc2b08fc484e8fa
Asma Hassan <asmasheikhhassan@gmail.com>
2023-04-20 20:05:22 +0100

    changed jquery code to js

--- recipe.js
+++ recipe.js
@@ -1,2 +1,23 @@
 var button = document.getElementById("search-button")
-var search = $("#search-input")
+var search = document.getElementById("search-input")
+
+
+
+
+button.addEventListener("click",function(event){
+    event.preventDefault();
+    // var currentVal = search.val();
+
+    getRecipeData;
+
+
+});
+
+
+async function getRecipeData(searchValue) {
+    // document.getElementById('#recipe-search"').style.display = 'block';
+    var response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public/?q=${searchValue}&app_id=96578cf4&app_key=ef31306002be0ec63784488c0c25e2ce&imageSize=REGULAR&imageSize=SMALL`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
