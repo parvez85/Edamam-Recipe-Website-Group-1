8e950bbfe96428edac044a78b4618aee264a851a
Asma Hassan <asmasheikhhassan@gmail.com>
2023-04-21 08:14:25 +0100

    Added cards and styled it. Added js code to show recipe name and ingredients

--- recipe.js
+++ recipe.js
@@ -8,24 +8,34 @@ var search = document.getElementById("search-input")
 button.addEventListener("click", function(event) {
     event.preventDefault();
     var searchValue = search.value;
-    getRecipeData(searchValue);
+
+    getRecipeData(searchValue).then(function(data){
+      displayRecipeData(data);
+    })
+    
+
   });
 
 
 async function getRecipeData(searchValue) {
-   // document.getElementsByClassName('card').style.display = 'block';
     var response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=96578cf4&app_key=ef31306002be0ec63784488c0c25e2ce&q=${searchValue}`)
-    console.log(response)
-   const data = await response.json();
+    const data = await response.json();
     console.log(data);
     return data;
+    
 }
 
 
 
-function displayRecipeData (data){
-    
-    var image = $('#image');
+function displayRecipeData(data){
+  
+    var ingredients1 = $('.ingredients');
+    var recipeTitle = $('.recipe-title');
+  
 
-    image.html(from.values(data).hits[0].recipe.images)
+    
+    recipeTitle.html(data.hits[0].recipe.label);
+    
+    ingredients1.html(data.hits[0].recipe.ingredientLines.join(", ")); //Maybe turn the ingredients into a list using innerhtml right now it is a block of text?
+  
 }
